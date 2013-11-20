95c8b752bd08be433b1dfb770b75b186c47a0693
LEE JAE HO <spectrick@gmail.com>
2013-11-20 18:56:04 +0900

    animate-css package 추가 및 적용

--- client/views/words/words.js
+++ client/views/words/words.js
@@ -1,8 +1,23 @@
+Template.words.helpers({
+  'ErrorAddWord':function() {
+    return Session.get('ErrorAddWord');
+  }
+});
+
 Template.words.events({
   "submit #wordPost": function() {
     Meteor.call('wordPost', {
       name: this.name,
       description: $("input[type=text].description").val()
+    }, function(err, result) {
+      Session.set('ErrorAddWord', err ? err.reason : void 0);
+      $("#error-add-word.fadeInUp").live('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        log.debug('end')
+        $(e.target).removeClass('fadeInUp').addClass('fadeOutUp');
+      });
+      $("#error-add-word.fadeOutUp").live('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        Session.set('ErrorAddWord');
+      });
     });
     $("input[type=text].description").val('');
     return false;
