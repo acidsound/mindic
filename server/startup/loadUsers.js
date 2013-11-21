c5dbb58983de2c1d5473a95e78eb278ce3b31f69
LEE JAE HO <spectrick@gmail.com>
2013-11-22 07:25:53 +0900

    role 기능 추가 roles collections 에 role명 추가 기본 유저 생성시 변경

--- server/startup/loadUsers.js
+++ server/startup/loadUsers.js
@@ -1,15 +1,18 @@
 function loadUser(user) {
-    var userAlreadyExists = typeof Meteor.users.findOne({ username : user.username }) === 'object';
+  var userAlreadyExists = typeof Meteor.users.findOne({ username: user.username }) === 'object';
 
-    if (!userAlreadyExists) {
-        Accounts.createUser(user);
-    }
+  if (!userAlreadyExists) {
+    log.debug('usercreate : ', db.user.username);
+    var id = Accounts.createUser(user);
+    Meteor.users.update({_id: id}, {$set:{'emails.0.verified': true}});
+    user.role && Roles.addUsersToRoles(id, user.role);
+  }
 }
 
 Meteor.startup(function () {
-    var users = YAML.eval(Assets.getText('users.yml'));
+  var users = YAML.eval(Assets.getText('users.yml'));
 
-    for (key in users) if (users.hasOwnProperty(key)) {
-        loadUser(users[key]);
-    }
+  for (key in users) if (users.hasOwnProperty(key)) {
+    loadUser(users[key]);
+  }
 });
