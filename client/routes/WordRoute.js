e903294fdce348a53227a80a43d0e161dd6100ec
JAEHO LEE <spectrick@gmail.com>
2013-11-27 02:46:09 +0900

    atoms 를 지정한 word 에 대해서만 한정하여 subscribe

--- client/routes/WordRoute.js
+++ client/routes/WordRoute.js
@@ -1,18 +1,18 @@
 WordController = RouteController.extend({
   template: 'word',
-  waitOn: function() {
+  waitOn: function () {
+    var word = Words.findOne({name: this.params.word, word_sequence: this.params.seq - 1}) || {_id: 0};
     return [
       Meteor.subscribe('word', this.params.word, this.params.seq),
-      Meteor.subscribe('atoms')
+      Meteor.subscribe('atoms_of_word', word._id)
     ];
   },
-  data: function() {
-    var _word = Words.find().count() && Words.findOne() || '';
-    log.debug(_word);
+  data: function () {
+    var _word = Words.findOne();
     return {
       name: this.params.word,
       word: _word,
-      atoms: Atoms.find({word_id: '' || _word._id})
+      atoms: Atoms.find({word_id: _word && _word._id || ''})
     }
   }
 });
